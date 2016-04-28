196dc5f5d4a0f39020949d0ceb33af8d576e5936
Martin Finkel <Martin Finkel>
2016-04-28 14:27:37 +0200

    New implementation of the Datahandling

--- handle_data.py
+++ handle_data.py
@@ -19,10 +19,11 @@ from glob import glob
 
 
 class Data(object):
-    def __init__(self, odf_phase_1_file=None, odf_phase_2_file=None):
+    def __init__(self, sample_diameter, odf_phase_1_file=None, odf_phase_2_file=None):
         self.data_list = []
         self.odf_phase_1 = self.get_odf(odf_phase_1_file)
         self.odf_phase_2 = self.get_odf(odf_phase_2_file)
+        self.sample_diameter = sample_diameter
 
     @staticmethod
     def get_odf(odf_file):
@@ -33,14 +34,22 @@ class Data(object):
         odf.read_data(path=path, filename=filename)
         return odf
 
+    def calc_applied_stress(self, force):
+        """
+        :param force: applied force in kN
+        :return: stress
+        """
+        area = (self.diameter * np.power(10., -3.)) ** 2 / 4 * np.pi
+        return force * np.power(10., 3) / area  # *np.power(10.,-9)
+
 
 class SPODIData(Data):
-    def __init__(self):
-        super(SPODIData, self).__init__()
+    def __init__(self, sample_diameter):
+        super(SPODIData, self).__init__(sample_diameter)
         self.data_dic_raw = {}  # a dictionary containing the raw data, the key is the applied force, the values
         # are lists of all measured orientation angles with the measured two_theta, intens, error data
         self.data_dic_phases = {}  # dictionary containing dictionaries of all peaks of the different phases
-        # data_dic_phases key=phase, val
+        # data_dic_phases key=phase, val=dic containing all reflexes (key = force)
 
     def load_data(self, list_of_dirs):
         """
@@ -108,14 +117,14 @@ class SPODIData(Data):
         data.close()
         return [TTheta, Intens, err]
 
-    def fit_the_peaks_for_on_diffraktin_pattern(self, data, peak_regions, phase, plot=False):
+    def fit_the_peaks_for_on_diffraktin_pattern(self, data, peak_regions, plot=False):
         """
         fitting the indiwidual peaks of each difraction pattern
         :param peak_regions:
         :return:
         """
         # hkl_2_theta = [[h, k, l, 2Theta, 2Theta_error, phase], [], ...
-        hkl_2_theta = np.zeros((len(peak_regions), 6))
+        hkl_2_theta = np.zeros((len(peak_regions), 5))
         for j in xrange(len(peak_regions)):
             hkl_2_theta[j][0] = peak_regions[j][0]
             hkl_2_theta[j][1] = peak_regions[j][1]
@@ -128,8 +137,26 @@ class SPODIData(Data):
             gauss = Fittingtools.gauss_lin_fitting_2(dax, day, day_err, plot=plot)
             hkl_2_theta[j][3] = gauss[0]
             hkl_2_theta[j][4] = gauss[1]
-            hkl_2_theta[j][5] = phase
         return hkl_2_theta
+
+    def fit_all_data(self, peak_regions_phase, plot=False):
+        # peak_region_phase is a list: [[phase_1, peak_region], [phase_2, peak_region]]
+
+        for i in peak_regions_phase:  # loop over all phases
+            phase, peak_regions = i
+            # self.data_dic_phases[phase] = list()
+            force_dic = {}
+            for j in self.data_dic_raw:  # loop over all forces
+                help_list = []
+                for n in self.data_dic_raw[j]:  # loop over all orientations
+                    force, omega, chi, two_theta, intens, error = n
+                    data = [two_theta, intens, error]
+                    hkl_2_theta = self.fit_the_peaks_for_on_diffraktin_pattern(data=data, peak_regions=peak_regions)
+                    hkl_2_theta = hkl_2_theta.tolist()
+                    for m in hkl_2_theta:
+                        help_list.append([phase, force, omega, chi, m])
+                force_dic[j] = help_list  # [[phase, force, omega, chi, h, k, l, 2theta, 2theta_error], ...]
+            self.data_dic_phases[phase] = force_dic
 
 
 class DataContainer(object):
