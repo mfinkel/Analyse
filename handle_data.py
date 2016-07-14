correct calculation of stress error

handle_data.py
@@ -51,7 +51,7 @@ class Data(object):
         area = (sample_diameter) ** 2 / 4 * np.pi
         force = force * np.power(10., 3)
         stress = force / area
-        stress_error = np.sqrt((force * 0.1 / ((self.sample_diameter / 2) ** 2 * np.pi)) ** 2
+        stress_error = np.sqrt((force * 0.02 / ((self.sample_diameter / 2) ** 2 * np.pi)) ** 2
                                + (2 * force / ((self.sample_diameter / 2) ** 3 * np.pi) * (
                                 self.sample_diameter / 2) * 0.05) ** 2)
         return stress, stress_error